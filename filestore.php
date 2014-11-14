3620d69126c36d232dfa97279c1b2ef68b3ac5e6
pkuzma101 <kuzma.paul@gmail.com>
2014-11-14 10:07:32 -0600

    Added comments and improved sytax of 'santize' and 'writeLines' functions

--- filestore.php
+++ filestore.php
@@ -12,33 +12,46 @@
       * Returns array of lines in $this->filename
       */
      function readLines() {
+     	// Creates variable that gets the size of the Todo List text file
      	$filesize = filesize($this->filename);
+     	// If the Todo List text file has anything in it...
 		if($filesize > 0) {
+			// Establishes variable that opens the Todo List text file and reads its content
 	        $handle = fopen($this->filename, "r");
+	        // Establishes variable that opens the text file as long as its not empty
 	        $contents = fread($handle, $filesize);
+	        // Makes Todo List text file contents into an array
 	        $contentArray = explode(PHP_EOL, $contents);
+	        // Closes the Todo List text file
 	        fclose($handle);
+	        // Returns the contents of the text file in array form
 	        return $contentArray;
 	    }
+	    // If there is nothing on the list
 	    else {
 	    	return ['test'];
 	    }
      }
 
-     public function sanitize() {
-		foreach ($this->items as $key => $value) {
-			$this->items[$key] = htmlspecialchars(strip_tags($value));
+     public function sanitize($array) {
+		foreach ($array as $key => $value) {
+			$array[$key] = htmlspecialchars(strip_tags($value));
 		}
-		return $this->items;
+		return $array;
 	}
      /**
       * Writes each element in $array to a new line in $this->filename
       */
-     function writeLines() {
-     	$array = $this->sanitize($this->items);
+     function writeLines($array) {
+     	// Strips out any heading tags or other bad stuff
+     	$array = $this->sanitize($array);
+     	// Establishes variable that will open Todo List text file and write in it
 	    $handle = fopen($this->filename, "w");
+	    // Makes list items into strings that can be placed on the list
 	    $string = implode("\n", $this->items);
+	    	// Opens the Todo List text file and writes in it
 	        fwrite($handle, $string);
+	        // Closes the Todo List text file
 	        fclose($handle);
      }
      /**
@@ -58,7 +71,6 @@
 				}
 			}
 			fclose($handle);
-			// $this->addresses = $addresses;
 		} 
 
 		return $addresses;
